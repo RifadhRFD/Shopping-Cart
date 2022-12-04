e95aab3ccc6ca2c494a42c88fcff37bdea35a56b
RifadhRFD <rifadhrfd@gmail.com>
2022-12-04 10:25:25 +0530

    initial commit of all files

---
+++ src/data.js
@@ -0,0 +1,31 @@
+
+let shopItemData = [
+    {
+        id: "aujsjjkj",
+        name: "Casual Shirt",
+        desc: "lorem lorem lorem lorem lorem lorem",
+        price: 45,
+        img: "images/img1.jpg"
+    },
+    {
+        id: "jdkskak",
+        name: "Office Shirt",
+        desc: "lorem lorem lorem lorem lorem lorem",
+        price: 55,
+        img: "images/img2.jpg"
+    },
+    {
+        id: "kmdsklajhc",
+        name: "Kids Shirt",
+        desc: "lorem lorem lorem lorem lorem lorem",
+        price: 65,
+        img: "images/img3.jpg"
+    },
+    {
+        id: "kjsdlas",
+        name: "Adult Shirt",
+        desc: "lorem lorem lorem lorem lorem lorem",
+        price: 75,
+        img: "images/img4.jpg"
+    }
+];
